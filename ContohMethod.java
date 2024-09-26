57a073e2e22d1856f6af9339e723afc2dfaff4dc
Acer <2382036@unai.edu>
2024-09-26 10:07:33 +0700

    Hanson4

--- ContohMethod.java
+++ ContohMethod.java
@@ -3,8 +3,10 @@ public class ContohMethod {
         int luas = sisi * sisi;
         return luas;
     }
-}
+
     public static void main(String[] args) {
-    int sisi = 5;
-    int luas = hitungLuasPersegi(int sisi)
+        int sisi = 5;
+        int luas = hitungPersegi(sisi); // remove "int" here
+        System.out.println("Luas persegi dengan sisi " + sisi + " adalah " + luas); // add "luas" here
     }
+}
